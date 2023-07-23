6296532281a549e04d2570498d1dde29b9011c16
BaVy <140328627+ivynails@users.noreply.github.com>
2023-07-23 21:21:07 +0700

    Tet chacha20.cpp
    only tet

--- src/crypto/chacha20.cpp
+++ src/crypto/chacha20.cpp
@@ -1,3 +1,4 @@
+// only for tet
 // Copyright (c) 2017-2022 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
